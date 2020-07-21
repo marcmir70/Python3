64381e52b889f4fdc573549f92194aecbdc155b9
Marcelo Miranda <marcelomiranda70@gmail.com>
2020-07-21 20:14:55 -0300

    14th exercise - common elements between two lists without duplicates

---
+++ PythonPro PythonBirds+Django/Practice_Python/ex14_list_remove_duplicates.py
@@ -0,0 +1,22 @@
+# Practice Python, Exercise 14 - from 20140515, done in 20190721
+# - ref: https://www.practicepython.org/exercise/2014/05/15/14-list-remove-duplicates.html
+# Exercise: takes a list and return a new one that contains all elements of first one minus all duplicates.
+# concepts: sets
+
+def list_duplicates_removal(list1,list2):
+    """ generate a set of common elements between two lists, without their duplicate ones
+    Ex:
+    >>> list_duplicates_removal([1, 1, 2, 3, 5, 8, 13, 21, 55, 89], [1, 2, 3, 3, 5, 6, 8, 9, 10, 12, 13])
+    {1, 2, 3, 5, 8, 13}
+
+    :param list1: list
+    :param list2: list
+    :return: set
+    """
+    common_elements = set()
+    for i in list1:
+        for j in list2:
+            if i == j:
+                common_elements.add(i)
+
+    print(common_elements)
