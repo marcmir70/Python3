3da638b4206a2531dec0d5493d6fbcd4b4dc0ab5
Marcelo Miranda <marcelomiranda70@gmail.com>
2020-07-21 21:26:46 -0300

    15th exercise - text with words in backwards order #hard (because minor errors, but - at least - I finished this one more) ;)

---
+++ PythonPro PythonBirds+Django/Practice_Python/ex15_reverse_word_order.py
@@ -0,0 +1,7 @@
+# Practice Python, Exercise 14 - from 20140515, done in 20190721 by Marcelo Miranda github.com/marcmir70
+# - ref: https://www.practicepython.org/exercise/2014/05/15/14-list-remove-duplicates.html
+# Exercise: ask user a long string containing multiple words. Then, print it with the words in backwards order.
+
+from functions_base.ex15_text_words_reversal import words_reversal
+
+words_reversal(input(f'Type me a phrase to show you it in reversal word order... '))
---
+++ PythonPro PythonBirds+Django/Practice_Python/functions_base/ex15_text_words_reversal.py
@@ -0,0 +1,24 @@
+# Practice Python, Exercise 14 - from 20140515, done in 20190721 by Marcelo Miranda github.com/marcmir70
+# - ref: https://www.practicepython.org/exercise/2014/05/15/14-list-remove-duplicates.html
+# Exercise: ask user a long string containing multiple words. Then, print it with the words in backwards order.
+
+def words_reversal(texto):
+    """ generates a text (string) with its words in reversal order
+    Ex:
+
+    >>> words_reversal('my name is Marcelo Miranda')
+    Miranda Marcelo is name my
+
+    :param texto: string
+    :return: message
+    """
+
+    text_words_list = texto.split()
+    text_words_reverted = []
+    for i in range(len(text_words_list)):
+        text_words_reverted.append(text_words_list[-(i+1)])
+
+    print(' '.join(text_words_reverted))
+
+    if __name__ == '__main__':
+        words_reversal('my name is Marcelo Miranda')
