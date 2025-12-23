775b0a902126df6397fa6618781a664f1e3aac63
Marc. Mir. <marcmir.std@gmail.com>
2025-12-23 15:56:28 -0300

    v3 (shebang, aula 63)

--- Udemy Python Basico ao Avançado/curso-python/fundamentos_projetos/area_circulo.py
+++ Udemy Python Basico ao Avançado/curso-python/fundamentos_projetos/area_circulo.py
@@ -1,13 +1,5 @@
-# v2
-# -*- coding: utf-8 -*-
-from string import Template
-
-pi = 3.14 # quando estava 3,14 repetia diversas vezes este valor (como string)
-raio = 15
-area = pi * (raio ** 2)
-
-msg = Template ('A área do círculo de raio $R é $A')
-
-print (msg.substitute(R=raio, A=area))
-
-# print (f'A área do círculo de raio {raio} é {area}')
+#!/usr/bin/python3
+# v3 - shebang (aula 63)
+pi = 3.14159 # quando estava 3,14 repetia diversas vezes este valor (como string)
+raio = 15.3
+print ('Área do círculo:', pi * raio ** 2)
