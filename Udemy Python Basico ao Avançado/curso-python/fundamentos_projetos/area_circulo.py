84caf6470e0b38c2a9f6852c3d051fc89b34eb24
Marc. Mir. <marcmir.std@gmail.com>
2025-12-30 14:07:16 -0300

    v9 function W/ return

--- Udemy Python Basico ao Avançado/curso-python/fundamentos_projetos/area_circulo.py
+++ Udemy Python Basico ao Avançado/curso-python/fundamentos_projetos/area_circulo.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-from math import pi  # v8 function wo/ return
+from math import pi  # v9 function w/ return
+
 
 def circulo(raio):
-    print('Area do círculo: ', pi * float(raio) ** 2)
+    return pi * float(raio) ** 2)
+
 
 if __name__ == '__main__':
     raio = input('Informe o raio: ')
-    circulo(raio)
+    area = circulo(raio)
+    print('Area do circulo: ', area)
