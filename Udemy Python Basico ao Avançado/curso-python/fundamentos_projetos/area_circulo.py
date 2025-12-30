d742565147a76f58ce2b27aa6f2c065eea8e4c3d
Marc. Mir. <marcmir.std@gmail.com>
2025-12-30 19:15:29 -0300

    v14 check numeric argument

--- Udemy Python Basico ao Avançado/curso-python/fundamentos_projetos/area_circulo.py
+++ Udemy Python Basico ao Avançado/curso-python/fundamentos_projetos/area_circulo.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-from math import pi  # v13 leaving w/ err
+from math import pi  # v14 check numeric argument
 import sys
 import errno
 
@@ -17,7 +17,12 @@ if __name__ == '__main__':
     if len(sys.argv) < 2 :
         help()
         sys.exit(errno.EPERM)   # code 1 ; not permitted operation ## sys.exit(1)
-    else:
-        raio = sys.argv[1]
-        area = circulo(raio)
-        print('Área do circulo: ', area)
+    
+    if not sys.argv[1].isnumeric():
+        help()
+        print('O raio deve ser um valor numérico')
+        sys.exit(errno.EINVAL)   # code 2 ; invalid argument
+    
+    raio = sys.argv[1]
+    area = circulo(raio)
+    print('Área do circulo: ', area)
