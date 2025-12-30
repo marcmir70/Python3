bda82cabd77e10fbef3aa57313b3237854e83feb
Marc. Mir. <marcmir.std@gmail.com>
2025-12-30 13:05:00 -0300

    v5 - getting data from user

--- Udemy Python Basico ao Avançado/curso-python/fundamentos_projetos/area_circulo.py
+++ Udemy Python Basico ao Avançado/curso-python/fundamentos_projetos/area_circulo.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
-# v4 - importando módulo Math (aula 64)
-# import math
-# print (dir()) # retorna ['__annotations__', ..., 'math']
+from math import pi  # v5 - obtendo dados do usuário
 
-from math import pi
-
-# print ('π = ', math.pi)
-# pi = 3.14159 # quando era 3,14 (com vírgula) repetia várias vezes essa str)
-raio = 15.3
-print ('Área do círculo:', pi * raio ** 2)
+raio = input('Informe o raio: ')
+print ('Área do círculo:', pi * float(raio) ** 2)
