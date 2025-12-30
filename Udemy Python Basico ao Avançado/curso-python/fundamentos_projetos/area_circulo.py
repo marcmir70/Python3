af0ad016b64c22c848000f05e030fcfbd6de4460
Marc. Mir. <marcmir.std@gmail.com>
2025-12-30 13:18:49 -0300

    v6 a bit about module

--- Udemy Python Basico ao Avançado/curso-python/fundamentos_projetos/area_circulo.py
+++ Udemy Python Basico ao Avançado/curso-python/fundamentos_projetos/area_circulo.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-from math import pi  # v5 - obtendo dados do usuário
+from math import pi  # v6 - a bit about module
 
 raio = input('Informe o raio: ')
 print ('Área do círculo:', pi * float(raio) ** 2)
+
+print('nome do módulo ', __name__)    # on VS Code returns __main__ ; on Python via import, area_circulo
+print('nome do pacote ', __package__) # on VS Code returns None
