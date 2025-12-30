1fd890e0d712d2303d4ff0ee5614f4716f34299e
Marc. Mir. <marcmir.std@gmail.com>
2025-12-30 13:50:47 -0300

    v7 main running module check

--- Udemy Python Basico ao Avançado/curso-python/fundamentos_projetos/area_circulo.py
+++ Udemy Python Basico ao Avançado/curso-python/fundamentos_projetos/area_circulo.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-from math import pi  # v6 - a bit about module
+from math import pi  # v7 main running module check
 
-raio = input('Informe o raio: ')
-print ('Área do círculo:', pi * float(raio) ** 2)
-
-print('nome do módulo ', __name__)    # on VS Code returns __main__ ; on Python via import, area_circulo
-print('nome do pacote ', __package__) # on VS Code returns None
+if __name__ == '__main__':
+    raio = input('Informe o raio: ')
+    print ('Área do círculo:', pi * float(raio) ** 2)
