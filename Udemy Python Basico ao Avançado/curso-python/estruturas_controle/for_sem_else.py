c1ccd429565993f81613baf236f2e2c08b9acce7
Marc. Mir. <marcmir.std@gmail.com>
2026-01-06 18:21:56 -0300

    FOR w/ and wo/ ELSE

---
+++ Udemy Python Basico ao Avançado/curso-python/estruturas_controle/for_sem_else.py
@@ -0,0 +1,15 @@
+PALAVRAS_PROIBIDAS = ('futebol', 'religião', 'política')
+textos = [
+    'João gosta de futebol e política', 
+    'A praia foi divertida',
+]
+
+for texto in textos:
+    found = False
+    for palavra in texto.lower().split():
+        if palavra in PALAVRAS_PROIBIDAS:
+            print('O texto possui, ao menos, uma palavra proibida:', palavra)
+            found = True
+            break
+    if not found:
+        print('Texto liberado:', texto)
