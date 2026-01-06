4d768ba40455cb584fa422eb602ca68ad531e6f8
Marc. Mir. <marcmir.std@gmail.com>
2026-01-06 17:09:24 -0300

    Match Case instead of emulated Switch

--- Udemy Python Basico ao Avançado/curso-python/estruturas_controle/switch_01.py
+++ Udemy Python Basico ao Avançado/curso-python/estruturas_controle/switch_01.py
@@ -1,14 +1,21 @@
 def get_dia_semana(dia):
-    dias = {
-        1: 'Domingo',
-        2: 'Segunda', 
-        3: 'Terça', 
-        4: 'Quarta', 
-        5: 'Quinta', 
-        6: 'Sexta', 
-        7: 'Sábado' 
-    }
-    return dias.get(dia, '** inválido **')
+    match dia:
+        case 1:
+            return 'Domingo'
+        case 2: 
+            return 'Segunda'
+        case 3: 
+            return 'Terça'
+        case 4: 
+            return 'Quarta'
+        case 5: 
+            return 'Quinta'
+        case 6: 
+            return 'Sexta'
+        case 7: 
+            return 'Sábado'
+        case _:
+            return '** inválido **'
 
 if __name__ == '__main__':
     for dia in range(0,9):
--- Udemy Python Basico ao Avançado/curso-python/estruturas_controle/switch_02.py
+++ Udemy Python Basico ao Avançado/curso-python/estruturas_controle/switch_02.py
@@ -1,14 +1,11 @@
 def get_tipo_dia(dia):
-    dias = {
-        1: 'Fim de semana',
-        2: 'Dia de semana', 
-        3: 'Dia de semana', 
-        4: 'Dia de semana', 
-        5: 'Dia de semana', 
-        6: 'Dia de semana', 
-        7: 'Fim de semana' 
-    }
-    return dias.get(dia, '** inválido **')
+    match dia:
+        case 1 | 7:
+            return 'Fim de semana'
+        case 2 | 3 | 4 | 5| 6:
+            return 'Dia de semana'
+        case _:
+            return '** inválido **'
 
 if __name__ == '__main__':
     for dia in range(0,9):
