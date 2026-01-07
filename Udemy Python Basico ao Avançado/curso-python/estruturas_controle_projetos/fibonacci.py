c2e5dc28277fb3ad17c15634731e4deb1208cd56
Marc. Mir. <marcmir.std@gmail.com>
2026-01-07 17:02:23 -0300

    v2 finite while loop (using limit)

--- Udemy Python Basico ao Avançado/curso-python/estruturas_controle_projetos/fibonacci.py
+++ Udemy Python Basico ao Avançado/curso-python/estruturas_controle_projetos/fibonacci.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-# 0, 1, 1, 2, 3, 5, 8, ...
-def fibonacci():
+# 0, 1, 1, 2, 3, 5, 8, ... ## v2 - finite while loop (using limit)
+def fibonacci(limite):
     penultimo = 0
     ultimo = 1
     print(f'{penultimo},{ultimo}', end=',')
-    while True: # execução sem término definido
+    while ultimo < limite:
         proximo = penultimo + ultimo
         print(proximo, end=',')
         penultimo = ultimo
         ultimo = proximo
 
 if __name__ == '__main__':
-    fibonacci()
+    fibonacci(10000)
