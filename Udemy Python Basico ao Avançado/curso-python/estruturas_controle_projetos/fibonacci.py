b2ad039b7cd5d3526c9cb3a7624dc02abadeee87
Marc. Mir. <marcmir.std@gmail.com>
2026-01-07 18:33:43 -0300

    v2.1 improvement: to really stop after the limit is reached

--- Udemy Python Basico ao Avançado/curso-python/estruturas_controle_projetos/fibonacci.py
+++ Udemy Python Basico ao Avançado/curso-python/estruturas_controle_projetos/fibonacci.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-# 0, 1, 1, 2, 3, 5, 8, ... ## v3 using Packing for variables exchange
+# 0, 1, 1, 2, 3, 5, 8, ... ## v2.1 improvement: to really stop after the limit is reached
 def fibonacci(limite):
     penultimo = 0
     ultimo = 1
     print(f'{penultimo},{ultimo}', end=',')
-    while ultimo < limite:
-        penultimo, ultimo = ultimo, penultimo + ultimo
-        print(ultimo, end=',')
+    while penultimo + ultimo < limite:
+        proximo = penultimo + ultimo
+        print(proximo, end=',')
+        penultimo = ultimo
+        ultimo = proximo
 
 if __name__ == '__main__':
-    fibonacci(10000)
+    fibonacci(6766)
