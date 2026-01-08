94565463c783dfeb50804dcf1b0a9fa26516a206
Marc. Mir. <marcmir.std@gmail.com>
2026-01-07 23:26:55 -0300

    v6 while w/ break

--- Udemy Python Basico ao Avançado/curso-python/estruturas_controle_projetos/fibonacci.py
+++ Udemy Python Basico ao Avançado/curso-python/estruturas_controle_projetos/fibonacci.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-# 0, 1, 1, 2, 3, 5, 8, ... ## v5 challenge: using SUM function
-def fibonacci(limite):
+# 0, 1, 1, 2, 3, 5, 8, ... ## v6 while w/ break
+def fibonacci(quantidade):
     resultado = [0, 1]
-    while resultado [-1] < limite:
+    while True:
         resultado.append(sum(resultado[-2:]))
+        if len(resultado) == quantidade:
+            break
     return resultado
 
 if __name__ == '__main__':
-    for fib in fibonacci(10000):
+    for fib in fibonacci(20): # para listar os 20 primeiros números
         print(fib)
