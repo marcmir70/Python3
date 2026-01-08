32be3f5c740615873d3366712ea6c8d164dbb978
Marc. Mir. <marcmir.std@gmail.com>
2026-01-07 23:37:17 -0300

    v7 FOR w/ RANGE (less loc -lines of code)

--- Udemy Python Basico ao Avançado/curso-python/estruturas_controle_projetos/fibonacci.py
+++ Udemy Python Basico ao Avançado/curso-python/estruturas_controle_projetos/fibonacci.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-# 0, 1, 1, 2, 3, 5, 8, ... ## v6 while w/ break
+# 0, 1, 1, 2, 3, 5, 8, ... ## v7 FOR w/ RANGE (less loc -lines of code)
 def fibonacci(quantidade):
     resultado = [0, 1]
-    while True:
+    for i in range(2, quantidade):
         resultado.append(sum(resultado[-2:]))
-        if len(resultado) == quantidade:
-            break
     return resultado
 
 if __name__ == '__main__':
