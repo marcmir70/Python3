4cab3b504d222cb26b6341696610b8a3e3faa31f
Marc. Mir. <marcmir.std@gmail.com>
2026-01-14 17:30:12 -0300

    v3 stream 2

--- Udemy Python Basico ao Avançado/curso-python/manipulacao_arquivos/io.py
+++ Udemy Python Basico ao Avançado/curso-python/manipulacao_arquivos/io.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 arquivo = open('pessoas.csv')
 for registro in arquivo:
-    print('Nome; {}, Idade: {}'.format(*registro.split(','))) # v2 stream 1
+    print('Nome; {}, Idade: {}'.format(*registro.strip().split(','))) # v3 stream 2
 arquivo.close
     
