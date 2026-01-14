0ee809d0bc40b2615534de7a4ad6f8bb8dfe3a3a
Marc. Mir. <marcmir.std@gmail.com>
2026-01-14 18:47:02 -0300

    v4 try/finally

--- Udemy Python Basico ao Avançado/curso-python/manipulacao_arquivos/io.py
+++ Udemy Python Basico ao Avançado/curso-python/manipulacao_arquivos/io.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
-arquivo = open('pessoas.csv')
-for registro in arquivo:
-    print('Nome; {}, Idade: {}'.format(*registro.strip().split(','))) # v3 stream 2
-arquivo.close
+try:
+    arquivo = open('pessoas.csv')
+    for registro in arquivo:
+        print('Nome; {}, Idade: {}'.format(*registro.strip().split(','))) # v4 try/finally
+# except IndexError:
+#     pass
+finally:
+    print('finally')
+    arquivo.close()
     
+if arquivo.closed:
+    print('Arquivo já fechado!')
