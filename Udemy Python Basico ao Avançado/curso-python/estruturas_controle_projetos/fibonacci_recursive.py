d17ff209284e3beab16ff81c37eef6c7400741bf
Marc. Mir. <marcmir.std@gmail.com>
2026-01-13 21:55:00 -0300

    using ternary operator

--- Udemy Python Basico ao Avançado/curso-python/estruturas_controle_projetos/fibonacci_recursive.py
+++ Udemy Python Basico ao Avançado/curso-python/estruturas_controle_projetos/fibonacci_recursive.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-def fibonacci(quantidade, sequencia = (0,1)):  # v1 recursive or 'v9'
+def fibonacci(quantidade, sequencia = (0,1)):  # v2 recursive - using ternary operator
     # importante: condição de parada
-    if len(sequencia) == quantidade:
-        return sequencia
-    return fibonacci(quantidade, sequencia + (sum(sequencia[-2:]),))
+    return sequencia if len(sequencia) == quantidade else \
+        fibonacci(quantidade, sequencia + (sum(sequencia[-2:]),))
                      
 if __name__ == '__main__':
     # lista os 20 primeiros números da sequência
