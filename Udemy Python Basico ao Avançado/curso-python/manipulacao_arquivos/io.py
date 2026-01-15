9db097bc5a0447e65ae5a6ceafc4ff089b71260c
Marc. Mir. <marcmir.std@gmail.com>
2026-01-14 21:26:55 -0300

    v6 writing a file f/ a CSV one

--- Udemy Python Basico ao Avançado/curso-python/manipulacao_arquivos/io.py
+++ Udemy Python Basico ao Avançado/curso-python/manipulacao_arquivos/io.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 with open('pessoas.csv') as arquivo:
-    for registro in arquivo:
-        print('Nome; {}, Idade: {}'.format(*registro.strip().split(','))) # v5 with
+    with open('pessoas.txt', 'w') as saida: # v6 writing a file f/ a CSV one
+        for registro in arquivo:
+            pessoa = registro.strip().split(',')
+            print('Nome; {}, Idade: {}'.format(*pessoa), file = saida) # v5 with
 
 if arquivo.closed:
     print('Arquivo já fechado!')
+
+if saida.closed:
+    print('Arquivo de saída já fechado!')
