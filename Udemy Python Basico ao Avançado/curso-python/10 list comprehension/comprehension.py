8600d3bc9ca047861c41314c72bbb21182ff3372
Marc. Mir. <marcmir.std@gmail.com>
2026-01-15 19:18:31 -0300

    v2

--- Udemy Python Basico ao Avançado/curso-python/10 list comprehension/comprehension.py
+++ Udemy Python Basico ao Avançado/curso-python/10 list comprehension/comprehension.py
@@ -1,9 +1,10 @@
-# [ expressao for item in list if condicional ]
-dobros = [i * 2 for i in range(10)] # (1,11)
-print(dobros)
+# [ expressao for item in list if condicional ] ## v2 for lesson 115
+dobros_dos_pares = [i * 2 for i in range(10) if i % 2 == 0]
+print(dobros_dos_pares)
 
 # versão "normal"
-dobros = []
+dobros_dos_pares = []
 for i in range(10):
-    dobros.append(i*2)
-print(dobros)
+    if i % 2 == 0:
+        dobros_dos_pares.append(i*2)
+print(dobros_dos_pares)
