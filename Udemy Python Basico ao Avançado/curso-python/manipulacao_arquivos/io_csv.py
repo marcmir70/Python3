824264c5d2a7f732c3981cdf82d7da7324c0a2a7
Marc. Mir. <marcmir.std@gmail.com>
2026-01-14 21:35:07 -0300

    CSV reader module

--- Udemy Python Basico ao Avançado/curso-python/manipulacao_arquivos/io_csv.py
+++ Udemy Python Basico ao Avançado/curso-python/manipulacao_arquivos/io_csv.py
@@ -1,5 +1,5 @@
 import csv
 
-with open('pessoas.csv') as entrada:
+with open('pessoas.csv') as entrada: # CSV reader module
     for pessoa in csv.reader(entrada):
         print('Nome: {}, Idade: {}'.format(*pessoa))
