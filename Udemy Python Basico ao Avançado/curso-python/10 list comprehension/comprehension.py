d8ec01d0f6e3b1b14eac7ca4d16557c430e888d5
Marc. Mir. <marcmir.std@gmail.com>
2026-01-15 19:54:13 -0300

    v3 generator

--- Udemy Python Basico ao Avançado/curso-python/10 list comprehension/comprehension.py
+++ Udemy Python Basico ao Avançado/curso-python/10 list comprehension/comprehension.py
@@ -1,10 +1,7 @@
-# [ expressao for item in list if condicional ] ## v2 for lesson 115
-dobros_dos_pares = [i * 2 for i in range(10) if i % 2 == 0]
-print(dobros_dos_pares)
-
-# versão "normal"
-dobros_dos_pares = []
-for i in range(10):
-    if i % 2 == 0:
-        dobros_dos_pares.append(i*2)
-print(dobros_dos_pares)
+generator = (i ** 2 for i in range(10) if i % 2 ==0) ## v3 generator
+print(next(generator)) # saida 0
+print(next(generator)) # saida 4
+print(next(generator)) # saida 16
+print(next(generator)) # saida 36
+print(next(generator)) # saida 64
+# print(next(generator)) # erro !
