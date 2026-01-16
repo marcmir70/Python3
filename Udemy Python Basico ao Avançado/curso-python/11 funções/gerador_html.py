4bf15e976c6ed5dff9ad208e13cc323995185254
Marc. Mir. <marcmir.std@gmail.com>
2026-01-16 16:47:31 -0300

    v3 using unpacking

--- Udemy Python Basico ao Avançado/curso-python/11 funções/gerador_html.py
+++ Udemy Python Basico ao Avançado/curso-python/11 funções/gerador_html.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
-def tag_bloco(texto, classe='success', inline=False): # v2 parâmetros nomeados
+def tag_bloco(conteudo, classe='success', inline=False): # v3 using unpacking
     tag = 'span' if inline else 'div'
-    return f'<{tag} class="{classe}">{texto}</{tag}>'
+    return f'<{tag} class="{classe}">{conteudo}</{tag}>'
+
+
+def tag_lista(*itens):
+    lista = ''.join(f'<li>{item}</li>' for item in itens)
+    return f'<ul>{lista}</ul>'
 
 
 if __name__ == '__main__':
     print(tag_bloco('bloco'))
     print(tag_bloco('inline e classe', 'info', True))
     print(tag_bloco('inline', inline=True))
-    print(tag_bloco(inline=True,texto='inline'))
-    print(tag_bloco('falhou', classe='error')) # ou print(tag_bloco('falhou', 'error'))
+    print(tag_bloco(inline=True,conteudo='inline'))
+    print(tag_bloco('falhou', classe='error'))
+    print(tag_bloco(tag_lista('Item 1', 'Item 2'),classe='info'))
