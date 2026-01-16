6ab556520cd6888d1a0dc230a97a4f6b6c69684e
Marc. Mir. <marcmir.std@gmail.com>
2026-01-15 21:18:14 -0300

    v4 generator w/ FOR

--- Udemy Python Basico ao Avançado/curso-python/10 list comprehension/comprehension.py
+++ Udemy Python Basico ao Avançado/curso-python/10 list comprehension/comprehension.py
@@ -1,7 +1,4 @@
-generator = (i ** 2 for i in range(10) if i % 2 ==0) ## v3 generator
-print(next(generator)) # saida 0
-print(next(generator)) # saida 4
-print(next(generator)) # saida 16
-print(next(generator)) # saida 36
-print(next(generator)) # saida 64
-# print(next(generator)) # erro !
+generator = (i ** 2 for i in range(10) if i % 2 ==0) ## v4 generator w/ FOR
+
+for numero in generator: 
+    print(numero)
