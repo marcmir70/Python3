c68ced3f1f90d70d02ebe5df88b7b81978713cac
Marc. Mir. <marcmir.std@gmail.com>
2026-01-16 11:12:49 -0300

    v2 parâmetros nomeados

--- Udemy Python Basico ao Avançado/curso-python/11 funções/gerador_html.py
+++ Udemy Python Basico ao Avançado/curso-python/11 funções/gerador_html.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-def tag_bloco(texto, classe='success'): # Ref.: anatomia de uma tag
-    return f'<div class="{classe}">{texto}</div>'
+def tag_bloco(texto, classe='success', inline=False): # v2 parâmetros nomeados
+    tag = 'span' if inline else 'div'
+    return f'<{tag} class="{classe}">{texto}</{tag}>'
 
 
 if __name__ == '__main__':
-    # Testes (assetions)
-    assert tag_bloco('Incluído com sucesso!') == \
-        '<div class="success">Incluído com sucesso!</div>'
-    assert tag_bloco('Impossível excluir!', 'error') == \
-        '<div class="error">Impossível excluir!</div>'
     print(tag_bloco('bloco'))
+    print(tag_bloco('inline e classe', 'info', True))
+    print(tag_bloco('inline', inline=True))
+    print(tag_bloco(inline=True,texto='inline'))
+    print(tag_bloco('falhou', classe='error')) # ou print(tag_bloco('falhou', 'error'))
