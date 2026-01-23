9c490544025295d05497140188c35353082ddf97
Marc. Mir. <marcmir.std@gmail.com>
2026-01-23 19:20:24 -0300

    Problem: solution using tuple

--- Udemy Python Basico ao Avançado/curso-python/11 funções/problema_arg_padrao_mutavel.py
+++ Udemy Python Basico ao Avançado/curso-python/11 funções/problema_arg_padrao_mutavel.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-def fibonacci(sequencia=[0,1]): # lista é algo mutável!
+# def fibonacci(sequencia=[0,1]): # lista é algo mutável!
+def fibonacci(sequencia=(0,1)): # agora tupla!
      # uso de mutáveis como valor default (armadilha)
-     sequencia.append(sequencia[-1] + sequencia[-2])
-     return sequencia
+        #  sequencia.append(sequencia[-1] + sequencia[-2])
+        #  return sequencia
+    return sequencia + (sequencia[-1] + sequencia[-2],)
 
 if __name__ == '__main__':
      inicio = fibonacci()
