3ff0a5509166dc43ad148420e30b7c5ae11ba0c4
Marc. Mir. <marcmir.std@gmail.com>
2026-02-01 19:19:13 -0300

    teacher solution for challeng (see the challenge for this code 1st version)

--- Udemy Python Basico ao Avançado/curso-python/11 funções/desafio_html.py
+++ Udemy Python Basico ao Avançado/curso-python/11 funções/desafio_html.py
@@ -1,26 +1,11 @@
 #!/usr/bin/python3
-def conteudo(params):               ### primeira solução de Marcelo Miranda
-    dados = ''                      # linhas de debug (aqui e abaixo): # print(f'type(params): {type(params)}')
-    if type(params) is tuple:       # print('aqui') # quando params vem de *args é tipo TUPLE; 
-        for texto in params:        # print(f'texto: {texto}')
-            dados += texto          # print(f'dados: {dados}')
-    elif type(params) is dict:      # quando params vem de kwargs é tipo DICT
-        dados = ' '.join(f'{chave.split("_")[-1]}="{valor}"'
-            for chave, valor in params.items())     # print(f'dados: {dados}')
-    return dados
-
-def tag(tag, *args, **kwargs): # args: conteúdo; kwargs: propriedades da tag
-                            # Debug... print(f'args: {args} / kwargs: {kwargs}')
-    if len(kwargs)== 0:
-        if len(args) == 0:
-            return 'no content'                                      # error!?
-        elif len(args) > 0:
-            return f'<{tag}>{conteudo(args)}</{tag}>'
-    elif len(kwargs) > 0:
-        if len(args) == 0:
-            return f'<{tag} {conteudo(kwargs)}>no content</{tag}>'  # error!?
-        elif len(args) > 0:
-            return f'<{tag} {conteudo(kwargs)}>{conteudo(args)}</{tag}>'
+def tag(tag, *args, **kwargs):  # solução do professor!
+    if 'html_class' in kwargs:  # kwargs = argumentos nomeados
+        kwargs['class'] = kwargs.pop('html_class')
+    attrs = ''.join(f'{k}="{v}" ' for k, v in kwargs.items())
+    # ou a forma de acima ou esta: # attrs = ' '.join(f'{k}="{v}"' for k, v in kwargs.items())
+    inner = ''.join(args)
+    return f'<{tag} {attrs}>{inner}</{tag}>'
 
 if __name__ == '__main__':
     print(
