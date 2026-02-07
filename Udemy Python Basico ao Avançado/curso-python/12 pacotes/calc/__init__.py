d0f5d2cc1880460d249c50505fac5e4841a77ab6
Marc. Mir. <marcmir.std@gmail.com>
2026-02-07 19:58:18 -0300

    v5 class 143 (package as facade)

---
+++ Udemy Python Basico ao Avançado/curso-python/12 pacotes/calc/__init__.py
@@ -0,0 +1,4 @@
+from pacote1.modulo1 import soma
+from pacote2.modulo1 import subtracao
+# works as a facade
+__all__ = ['soma', 'subtracao' ]
--- Udemy Python Basico ao Avançado/curso-python/12 pacotes/pacote.py
+++ Udemy Python Basico ao Avançado/curso-python/12 pacotes/pacote.py
@@ -1,5 +1,4 @@
-from pacote1 import modulo1
-from pacote2 import modulo1 as modulo1_sub
-# v3 class 141 (modules with same name)
-print('Soma',modulo1.soma(3,2))
-print('Subtração',modulo1_sub.subtracao(3,2))
+from calc import soma, subtracao
+# v5 class 143 (package as facade)
+print('Soma', soma(3,2))
+print('Subtração', subtracao(3,2)) 
