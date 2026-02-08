f50a35479b0a647aab44010948e3b8d417239fd1
Marc. Mir. <marcmir.std@gmail.com>
2026-02-07 21:27:13 -0300

    class 144 package challenge

---
+++ Udemy Python Basico ao Avançado/curso-python/12 pacotes/desafio_package.py
@@ -0,0 +1,12 @@
+from app.utils.gerador import novo_nome
+from app.negocio import nome_existe
+from app.negocio.backend import add_nome
+
+def main():
+    while True:
+        nome = novo_nome() # retorna uma string a cada vez que executar
+        if not nome_existe(nome): # pode sempre retornar False
+            add_nome(nome) # não precisa fazer nada / pode ser bloco vazio
+            break
+
+    print(f'Criado novo nome de testes: "{nome}"')
