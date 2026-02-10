80f23c7f894d6c3608116629f8b4024b48453fc2
Marc. Mir. <marcmir.std@gmail.com>
2026-02-10 18:22:43 -0300

    v1 class 148 (poo - class members)

---
+++ Udemy Python Basico ao Avançado/curso-python/13 poo/data.py
@@ -0,0 +1,20 @@
+class Data:
+    # def to_str(self):
+    def __str__(self):
+        ## pass
+        return f'{self.dia}/{self.mes}/{self.ano}'
+
+
+d1 = Data() 
+d1.dia = 5
+d1.mes = 12
+d1.ano = 2019
+# print(d1.to_str())
+print(d1)
+
+d2 = Data() 
+d2.dia = 7
+d2.mes = 11
+d2.ano = 2020
+# print(d2.to_str())
+print(d2)
