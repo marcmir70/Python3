cfab4431f7d6458730159144cf1e8b9bc0078a14
Marc. Mir. <marcmir.std@gmail.com>
2026-02-10 21:39:33 -0300

    v2  class 150 (construtor - v2.1)

--- Udemy Python Basico ao Avançado/curso-python/13 poo/data.py
+++ Udemy Python Basico ao Avançado/curso-python/13 poo/data.py
@@ -1,18 +1,17 @@
 class Data:
-    # def to_str(self):
+    def __init__(self):
+        print('opa')
     def __str__(self):
-        ## pass
         return f'{self.dia}/{self.mes}/{self.ano}'
 
-
-d1 = Data() 
+d1 = Data()
 d1.dia = 5
 d1.mes = 12
 d1.ano = 2019
 # print(d1.to_str())
 print(d1)
 
-d2 = Data() 
+d2 = Data()
 d2.dia = 7
 d2.mes = 11
 d2.ano = 2020
