9f2dc2b2f5d81679792c251414db4fe2079b78e2
Marc. Mir. <marcmir.std@gmail.com>
2026-02-11 23:37:07 -0300

    class 152 OOP class Carro challenge my answer

--- Udemy Python Basico ao Avançado/curso-python/13 poo/desafio_carro.py
+++ Udemy Python Basico ao Avançado/curso-python/13 poo/desafio_carro.py
@@ -1,11 +1,36 @@
 # criar classe Carro, e seus atributos e comportamentos
 # acelerar : padrão=5, até no máximo de 180 (se a 178, poderá acelerar só 2)
 # frear : no máximo, até 0, no máximo (se estiver a 3, poderá frear só 17)
+veloc = 0
+
+class Carro:
+    def __init__(self, maxVeloc):
+        self.maxVeloc = maxVeloc
+
+    def acelerar(self, delta=5):
+        global veloc
+        if veloc + delta > self.maxVeloc:
+            veloc = self.maxVeloc
+        else:
+            veloc += delta
+        return veloc
+
+    def frear(self, delta):
+        global veloc 
+        self.delta = delta
+        if veloc - self.delta < 0:
+            veloc = 0
+        else:
+            veloc -= delta
+        return veloc
 
 if __name__ == '__main__':
-    c1 = Carro(180)   # velocidade máxima
-    for _ in range(25):
-        print(c1.acelerar(8))
 
-    for _ in range(10):
+    c1 = Carro(180)   # velocidade máxima
+    for a in range(25):
+        print(c1.acelerar(8))
+    
+    print('...')
+
+    for f in range(10):
         print(c1.frear(delta=20))
