44a41e50bfb3631a808b08cb5c8a7ffb20b35be3
Marc. Mir. <60441153+marcmir70@users.noreply.github.com>
2026-02-14 11:59:53 -0300

    versão desafio

---
+++ Udemy Python Basico ao Avançado/curso-python/13 poo/todo.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+from datetime import datetime
+
+class Tarefa():
+    def __init__(self,descricao):
+        self.descricao = descricao
+        self.feito = False
+        self.criacao = datetime.now()
+
+    def concluir(self):
+        return self.descricao + (' Concluída)') if self.feito else ('')
+
+def main():
+    casa = []
+    casa.append(Tarefa('Passar roupa'))
+    casa.append(Tarefa('Lavar prato'))
+
+    # Desafio: percorrer todas as tarefas, usando o método concluir só para "Lavar prato"
