6b1d0131cf9cad879ff931757be4738811792acf
marcmir70 <marcmir70@users.noreply.github.com>
2026-02-23 22:13:39 -0300

    v2 lesson 154 (fixes) (Classe Projeto)

--- Udemy Python Basico ao Avançado/curso-python/13 poo/todo.py
+++ Udemy Python Basico ao Avançado/curso-python/13 poo/todo.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 from datetime import datetime
 
-class Projeto():
-    def __init__f(self, nome):
+class Projeto:
+    def __init__(self, nome):
         self.nome = nome
         self.tarefas = []
 
@@ -14,20 +14,24 @@ class Projeto():
 
     def procurar(self, descricao):
         # Possível IndexError - para descrição inexistente
-        return [tarefa for tarefa in self.tarefa:
+        return [tarefa for tarefa in self.tarefas
                 if tarefa.descricao == descricao][0]
 
     def __str__(self):
-        return f'{self.nome} ({len(self.pendentes())} tarefas pendentes)'
+        return f'{self.nome} ({len(self.pendentes())} tarefa(s) pendente(s))'
     
 class Tarefa():
-    def __init__(self,descricao):
+    def __init__(self, descricao):
         self.descricao = descricao
         self.feito = False
         self.criacao = datetime.now()
 
     def concluir(self):
-        return self.descricao + (' Concluída)') if self.feito else ('')
+        self.feito = True
+
+    def __str__(self):
+        return self.descricao + (' (Concluída)' if self.feito else '')
+    
 
 def main():
     # casa = []
@@ -38,7 +42,7 @@ def main():
     # for tarefa in casa:
     #     print(f'- {tarefa}')
 
-    casa = Projeto('Tarefa de Casa
+    casa = Projeto('Tarefas de Casa')
     casa.add('Passar roupa')
     casa.add('Lavar prato')
     print(casa)
